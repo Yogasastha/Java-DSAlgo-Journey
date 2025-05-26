0e139433669a35ed1d67a5f59ba82e2acf62b54e
Yogasastha K <131787802+Yogasastha@users.noreply.github.com>
2025-05-26 23:06:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+
+        
+        for (int i = 0; i < n; i++) {
+            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
+                int correctIndex = arr[i] - 1;
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        
+        return n + 1;
+    }
+}
