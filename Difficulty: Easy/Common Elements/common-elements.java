dc44c81e32b2aa1265a5f932b52ab280ba3b13c6
Yogasastha K <131787802+Yogasastha@users.noreply.github.com>
2025-07-13 22:43:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common Elements/common-elements.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+
+class Solution {
+    public static ArrayList<Integer> commonElements(int a[], int b[]) {
+         Map<Integer, Integer> freqMap = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int num : a) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        
+        for (int num : b) {
+            if (freqMap.containsKey(num) && freqMap.get(num) > 0) {
+                result.add(num);
+                freqMap.put(num, freqMap.get(num) - 1);
+            }
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+}
