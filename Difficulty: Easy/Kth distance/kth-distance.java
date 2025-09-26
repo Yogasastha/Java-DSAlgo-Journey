8e3c0e5fe262e38b28b2bf36fcf4f745a89bb421
Yogasastha K <131787802+Yogasastha@users.noreply.github.com>
2025-09-26 20:19:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+    public boolean checkDuplicatesWithinK(int[] arr, int k) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i=0; i<arr.length; i++)
+        {
+
+            if (set.contains(arr[i]))
+               return true;
+
+            set.add(arr[i]);
+
+            if (i >= k)
+              set.remove(arr[i-k]);
+        }
+        return false;
+    }
+}
